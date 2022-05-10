340a0c1247105e88b642800272e3a479cc65436e
meghagarg217 <megha1268.cse19@chitkara.edu.in>
2022-05-10 22:42:30 +0530

    Create ValidParenthesesCpp

---
+++ Strings/ValidParentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<int> temp;
+        for(int i=0;i<n;i++) {
+            if(temp.empty()) {
+                temp.push(s[i]);
+            }
+            else if((temp.top()=='(' && s[i]==')') || (temp.top()=='[' && s[i]==']') || (temp.top()=='{' && s[i]=='}')) 
+            {
+                temp.pop();
+            }
+            else {
+                temp.push(s[i]);
+            }
+        }
+        if(temp.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
